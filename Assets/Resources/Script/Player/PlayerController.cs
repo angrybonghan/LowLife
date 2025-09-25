a67733e14fef41c67841555b947b968c712aadef
LSH005 <isihyeon005shingu@gmail.com>
2025-09-26 03:37:56 +0900

    [feat] 대기, 달리기 애니메이션 구현

--- Assets/Resources/Script/Player/PlayerController.cs
+++ Assets/Resources/Script/Player/PlayerController.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    public float acceleration = 5;
-    public float decelerationMultiplier = 5;
-    public float maxSpeed = 15;
-    public float jumpForce = 16;
+    public float acceleration = 5;  // 플레이어 가속도
+    public float decelerationMultiplier = 5;    // 가속도에 배수로 작용하는 감속도
+    public float maxSpeed = 15; // 최고 속도
+    public float jumpForce = 10;    // 점프 힘
 
     [Header("지상 감지 위치")]
     public Transform groundCheckLeft;
@@ -20,25 +20,30 @@ public class PlayerController : MonoBehaviour
     private float layerCheckRadius = 0.05f;  // 감지 위치 반경
 
     private bool isFacingRight = true;  // 오른쪽을 바라보고 있는가? (방향 전환에 필요함)
-    private bool isRunning = false; // 움직이는 중인가? (속도 상관 없음)
+    private bool isRunning = false; // 움직이는 중인가?
+    private bool hasInput = false; // 입력이 있는가? (A 또는 D를 눌렀을 때 true. 단, 동시에 누르는 것을 제외함.)
     private bool isGrounded = false;    // 땅에 닿았는가? (점프, 월런 해제에 이용)
 
     private Rigidbody2D rb;
+    private Animator anim;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
 
 
     void Update()
     {
-        UpdateStates();
-        MoveInputHandler();
-        CheckFlip();
-        HandleMovement();
-        JumpHandler();
+        UpdateStates(); // 상태 업데이트
+        MoveInputHandler(); // 조작 키 감지
+
+        CheckFlip();    // 캐릭터 좌우 회전
+        HandleMovement();   // 감지된 키를 기반으로 움직임
+        JumpHandler();  // 점프
+        UpdateAnimation(); // 애니메이션 업데이트
     }
 
     void CheckFlip()
@@ -60,13 +65,12 @@ public class PlayerController : MonoBehaviour
             else moveInput = 1;
 
             lastMoveInput = moveInput;
-            isRunning = true;
+            hasInput = true;
         }
         else
         {
             moveInput = 0;
-
-            isRunning = false;
+            hasInput = false;
         }
     }
 
@@ -80,7 +84,7 @@ public class PlayerController : MonoBehaviour
 
     void HandleMovement()
     {
-        if (isRunning)
+        if (hasInput)
         {
             currentMoveSpeed = Mathf.Min(currentMoveSpeed + acceleration * Time.deltaTime, maxSpeed);
         }
@@ -104,10 +108,28 @@ public class PlayerController : MonoBehaviour
 
     void UpdateStates()
     {
+        // 움직이는지 확인
+        if (currentMoveSpeed <= 0)
+        {
+            isRunning = false;
+        }
+        else
+        {
+            isRunning = true;
+        }
+        
+
+        // 지상인지 확인
         bool leftFoot = Physics2D.OverlapCircle(groundCheckLeft.position, layerCheckRadius, groundLayer);
         bool centerFoot = Physics2D.OverlapCircle(groundCheckCenter.position, layerCheckRadius, groundLayer);
         bool rightFoot = Physics2D.OverlapCircle(groundCheckRight.position, layerCheckRadius, groundLayer);
         isGrounded = leftFoot || centerFoot || rightFoot;
+    }
+
+    void UpdateAnimation()
+    {
+        anim.SetBool("isRunning", isRunning);
+        anim.SetFloat("moveSpeed", currentMoveSpeed / maxSpeed);
     }
 
     void OnDrawGizmosSelected()
