505daef5d1e1f12bd258c53c34249d165569c8d7
LSH005 <isihyeon005shingu@gmail.com>
2025-10-30 00:02:17 +0900

    [feat] Ms 의 이동, 플레이어 감지 구현

--- Assets/Script/Entity/Monster/Ms/MsMovement.cs
+++ Assets/Script/Entity/Monster/Ms/MsMovement.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(Animator), typeof(BoxCollider2D))]
@@ -9,11 +10,16 @@ public class MsMovement : MonoBehaviour
     public float trunDuration = 0.5f;   // 회전 대기 시간
     public float acceleration = 2f; // 가속도
 
+    [Header("팔, 머리")]
+    public Transform headObj;
+    public Transform armObj;
+
     [Header("지형 감지")]
     public Transform wallCheckPos;  // 벽
     public Transform groundCheckPos;    // 땅
 
     [Header("공격")]
+    public Transform firePoint;
     public float readyToAttackTime = 0.5f;  // 공격의 준비 시간 (총 들기, 내리기)
     public float aimingTime = 0.2f;          // 조준 시간 (이후 발사)
     public float reloadTime = 0.5f;  // 공격의 준비 시간 (재장전)
@@ -53,6 +59,7 @@ public class MsMovement : MonoBehaviour
     private bool canGoStraight = true;  // 직진 가능 여부 (벽이 없고 땅이 있어야 함)
     private bool isMoving = false;  // 움직이고 있는지 여부
     private bool isDead = false;    // 죽었는지 여부
+    private bool isAiming = false;
 
     Vector3 movePosRight;
     Vector3 movePosLeft;
@@ -90,7 +97,25 @@ public class MsMovement : MonoBehaviour
 
     void Update()
     {
-        
+        UpdateStates();
+
+        switch (currentState)
+        {
+            case state.idle:
+                if (IsPlayerInView())
+                {
+                    SetState(state.doubt);
+                }
+
+                break;
+
+            case state.track:
+                TrackHandler();
+
+                break;
+        }
+
+        if (isAiming) RotatePartsToLookAtPlayer();
     }
 
     void SetState(state targetState)
@@ -109,16 +134,18 @@ public class MsMovement : MonoBehaviour
 
             targetPos = isFacingRight ? movePosRight : movePosLeft;
 
+            StartCoroutine(IdleMovement());
         }
         else if (targetState == state.doubt)
         {
             exclamationMark = Instantiate(exclamationMarkObj).GetComponent<ExclamationMarkHandler>();
             exclamationMark.SetTargetPos(exclamationMarkPos);
 
+            StartCoroutine(DoubtHandler());
         }
         else if (targetState == state.attack)
         {
-
+            StartCoroutine(AttackHandler());
         }
         else if (targetState == state.endAttack)
         {
@@ -126,9 +153,242 @@ public class MsMovement : MonoBehaviour
         }
     }
 
+    IEnumerator IdleMovement()
+    {
+        while (true)
+        {
+            float sign = isFacingRight ? 1f : -1f;
+
+            while (!HasArrived(targetPos) && canGoStraight)
+            {
+                currentNormalizedSpeed = Mathf.Min(currentNormalizedSpeed + acceleration * Time.deltaTime, 0.5f);
+                rb.velocity = new Vector2(sign * currentNormalizedSpeed * maxSpeed, rb.velocity.y);
+                yield return null;
+            }
+            rb.velocity = Vector3.zero;
+            currentNormalizedSpeed = 0;
+
+            yield return new WaitForSeconds(trunDuration);
+            SwitchPos();
+            yield return null;
+        }
+    }
+
+    IEnumerator DoubtHandler()
+    {
+        float TimeSincePlayerLost = 0;
+        while (true)
+        {
+            if (IsPlayerInView()) detectionRate += Time.deltaTime / detectionTime;
+            else detectionRate -= Time.deltaTime / detectionDecayTime;
+
+            detectionRate = Mathf.Clamp01(detectionRate);
+            exclamationMark.SetGaugeValue(detectionRate);
+
+            if (detectionRate == 1)
+            {
+                anim.SetTrigger("readyToAttack");
+                yield return new WaitForSeconds(readyToAttackTime);
+
+                SetState(state.track);
+            }
+
+            if (detectionRate == 0)
+            {
+                TimeSincePlayerLost += Time.deltaTime;
+                if (TimeSincePlayerLost >= detectionCancelTime)
+                {
+                    Destroy(exclamationMark.gameObject);
+                    SetState(state.idle);
+                }
+            }
+            else
+            {
+                TimeSincePlayerLost = 0;
+                LookPos(playerObject.transform.position);
+            }
 
 
+            yield return null;
+        }
+    }
 
+    void TrackHandler()
+    {
+        LookPos(playerObject.transform.position);
+
+        Vector2 checkPos = playerObject.transform.position;
+        checkPos.y = transform.position.y;
+
+        if (canGoStraight && !HasArrived(checkPos))
+        {
+            currentNormalizedSpeed = Mathf.Clamp(currentNormalizedSpeed + acceleration * Time.deltaTime, 0.505f, 1f);
+            rb.velocity = new Vector2(facingSign * currentNormalizedSpeed * maxSpeed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = Vector3.zero;
+            currentNormalizedSpeed = 0;
+        }
+
+        if (IsPlayerInRange())
+        {
+            SetState(state.attack);
+            return;
+        }
+
+        if (IsPlayerInView())
+        {
+            detectionRate += Time.deltaTime / detectionTime;
+        }
+        else
+        {
+            detectionRate -= Time.deltaTime / detectionDecayTime;
+        }
+
+        detectionRate = Mathf.Clamp01(detectionRate);
+        exclamationMark.SetGaugeValue(detectionRate);
+
+        if (detectionRate == 0)
+        {
+            SetState(state.endAttack);
+            Destroy(exclamationMark.gameObject);
+        }
+    }
+
+    IEnumerator AttackHandler()
+    {
+        while (true)
+        {
+            anim.SetTrigger("aiming");
+            isAiming = true;
+            yield return new WaitForSeconds(aimingTime);
+            isAiming = false;
+            anim.SetTrigger("fire");
+            Attack();
+            yield return new WaitForSeconds(reloadTime);
+
+            if (!IsPlayerInRange()) break;
+        }
+
+        SetState(state.track);
+    }
+
+    void Attack()
+    {
+
+    }
+
+
+    void RotatePartsToLookAtPlayer()
+    {
+        Vector3 targetDirection = playerObject.transform.position - transform.position;
+        float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg;
+
+        float targetArmAngle = angle;
+
+        if (!isFacingRight) targetArmAngle = 180f - targetArmAngle;
+
+        Quaternion armRotation = Quaternion.Euler(0, 0, targetArmAngle);
+        armObj.localRotation = armRotation;
+
+        float finalHeadAngle = targetArmAngle * 0.5f;
+
+        Quaternion headRotation = Quaternion.Euler(0, 0, finalHeadAngle);
+        headObj.localRotation = headRotation;
+    }
+
+    bool IsPlayerInView()
+    {
+        Vector2 localAdjustedOffset = new Vector2(viewOffset.x * facingSign, viewOffset.y);
+        Vector2 worldCenter = (Vector2)transform.position + localAdjustedOffset;
+
+        Collider2D[] hitTargets = Physics2D.OverlapBoxAll(
+            worldCenter,            // 중심 위치
+            viewSize,             // 크기
+            0f,                     // 회전 각도
+            playerLayer             // 감지할 레이어
+        );
+
+        bool isPlayerInView = false;
+
+        if (hitTargets.Length > 0)
+        {
+            foreach (Collider2D targetCollider in hitTargets)
+            {
+                if (targetCollider.CompareTag("Player"))
+                {
+                    isPlayerInView = true;
+                    break;
+                }
+            }
+        }
+
+        if (!isPlayerInView) return false;
+
+        Vector2 startPos = transform.position;
+        Vector2 endPos = playerObject.transform.position;
+        Vector2 direction = (endPos - startPos).normalized;
+        float distance = Vector3.Distance(startPos, endPos);
+
+        RaycastHit2D hit = Physics2D.Raycast(startPos, direction, distance, obstacleMask);
+        if (hit.collider != null) return false;
+        return true;
+    }
+
+    bool IsPlayerInRange()
+    {
+        return Vector3.Distance(transform.position, playerObject.transform.position) < attackRange;
+    }
+
+    void SwitchPos()
+    {
+        Flip();
+
+        targetPos = isFacingRight ? movePosRight : movePosLeft;
+    }
+
+    void LookPos(Vector2 targetPos)
+    {
+        float directionX = targetPos.x - transform.position.x;
+
+        if (directionX != 0 && (directionX > 0) != isFacingRight)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+
+        facingSign = isFacingRight ? 1 : -1;
+
+        UpdateStates();
+    }
+
+    bool HasArrived(Vector3 pos)
+    {
+        float distance = Vector3.Distance(transform.position, pos);
+        return distance <= 0.1f;
+    }
+
+    void UpdateStates()
+    {
+        movePosRight.y = movePosLeft.y = targetPos.y = transform.position.y;
+
+        bool isGrounded = Physics2D.OverlapCircle(groundCheckPos.position, layerCheckRadius, obstacleMask);
+        bool isTouchingAnyWall = Physics2D.OverlapCircle(wallCheckPos.position, layerCheckRadius, obstacleMask);
+
+        canGoStraight = isGrounded && !isTouchingAnyWall;
+
+        isMoving = currentNormalizedSpeed > 0f;
+
+        anim.SetBool("isMoving", isMoving);
+        anim.SetFloat("moveSpeed", currentNormalizedSpeed);
+
+    }
 
     private void OnDrawGizmosSelected()
     {
