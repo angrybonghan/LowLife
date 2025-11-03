77a514bc7d7be2cd441109f61656e556f507cba8
LSH005 <isihyeon005shingu@gmail.com>
2025-11-03 19:00:25 +0900

    [feat] 다이얼로그 SO 구조 구현

---
+++ Assets/Script/Game/Dialogue SO/DialogueSO.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+[System.Serializable]
+public class DialogueLine
+{
+    [Header("말풍선 위치")]
+    public Vector3 bubblePosition;  // 말풍선의 월드 좌표
+    // bubblePositionOffset 이 원점 (0,0,0) 이라면 말풍선 위치를 움직이지 않음
+    public float bubbleBodyOffset;  // 말풍선 몸통의 좌-우 오프셋
+    public bool isTailDown;   // 말풍선 꼬리표가 위쪽에 있을지에 대한 여부
+
+    [Header("텍스트")]
+    [TextArea(1, 10)]   // 인스펙터 창에서 표시되는 입력란 열 최소, 최대 크기
+    public string[] sentence;    // 대사 내용 배열 (입력되지 않으면 문자열을 넘기거나, 다음 세션으로 넘어감)
+
+    [Header("설정")]
+    public float intervalTime; // 글자 하나의 출력 간격 시간
+    public bool canSkip; // 플레이어가 이 대사를 임의의 지정된 키로 넘길 수 있는지에 대한 여부. true면 넘길 수 있음.
+    public bool autoSkip; // 자동 스킵의 여부. true면 문자열 하나가 전부 재생되었을 때, autoSkipIntervalTime 초 대기 후 자동으로 다음 대사로 넘어감
+    public float autoSkipIntervalTime; // 자동 스킵에서 대기할 시간
+    public AudioClip[] typingSound;   // 글자 하나가 나올 때마다 재생될 랜덤 소리 (입력되지 않으면 재생되지 않음)
+}
+
+[CreateAssetMenu(fileName = "Totally Awesome Dialogue", menuName = "Dialogue/New Dialogue")]
+public class DialogueSO : ScriptableObject
+{
+    public DialogueLine[] section;
+}
---
+++ Assets/Script/Test/DialogueTest.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class DialogueTest : MonoBehaviour
+{
+    public DialogueSO someTuffDialogue;
+    private void Update()
+    {
+        
+    }
+}
