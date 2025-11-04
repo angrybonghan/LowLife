463dd90d17b09c5fbace1b3c8ed119e7e48d5ce8
LSH005 <isihyeon005shingu@gmail.com>
2025-11-04 21:23:03 +0900

    [feat] 상호작용에서 다이얼로그를 호출할 수 있는 기능 구현

---
+++ Assets/Script/Game/StartDialogueWithInteraction.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using UnityEngine;
+
+public class StartDialogueWithInteraction : MonoBehaviour, I_Interactable, I_DialogueCallback
+{
+    [Header("대화")]
+    public DialogueSO dialogue;
+
+    [Header("플레이어")]
+    public bool movePlayer;
+    public Vector3 playerPos;
+    public float duration = 0.2f;
+    public bool facingRight = true;
+
+
+    public void InInteraction()
+    {
+        if (dialogue == null) return;
+
+        if (movePlayer)
+        {
+            PlayerHandler.PlayerGoto(playerPos, duration, facingRight);
+            PlayerController.instance.AllStop();
+        }
+
+        DialogManager.instance.StartDialogue(dialogue, gameObject);
+
+        PlayerController.canControl = false;
+    }
+
+    public void OnDialogueEnd()
+    {
+        StartCoroutine(DialogueEndFunc());
+    }
+
+    IEnumerator DialogueEndFunc()
+    {
+        yield return new WaitForSeconds(0.2f);
+
+        PlayerController.canControl = true;
+
+        if (TryGetComponent<InteractionManager>(out InteractionManager interactionManager))
+        {
+            interactionManager.ResetInteraction();
+        }
+    }
+
+}
