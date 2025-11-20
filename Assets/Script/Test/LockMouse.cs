2928a67f263eee3228c02eaac1fc907085a64575
LSH005 <isihyeon005shingu@gmail.com>
2025-11-21 00:23:27 +0900

    [feat] LockMouse 스크립트가 시작에서 즉시 마우스를 잠글 수 있는 기능 구현

--- Assets/Script/Test/LockMouse.cs
+++ Assets/Script/Test/LockMouse.cs
@@ -2,24 +2,40 @@ using UnityEngine;
 
 public class LockMouse : MonoBehaviour
 {
+    [Header("¼³Á¤")]
+    public bool lockMouseAtStart;
+
     private bool isMouseLocked = false;
+
+    private void Start()
+    {
+        if (lockMouseAtStart)
+        {
+            LockmodeSwitch();
+        }
+    }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            isMouseLocked = !isMouseLocked;
+            LockmodeSwitch();
+        }
+    }
 
-            if (isMouseLocked)
-            {
-                Cursor.visible = false;
-                Cursor.lockState = CursorLockMode.Confined;
-            }
-            else
-            {
-                Cursor.visible = true;
-                Cursor.lockState = CursorLockMode.None;
-            }
+    void LockmodeSwitch()
+    {
+        isMouseLocked = !isMouseLocked;
+
+        if (isMouseLocked)
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+        else
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
         }
     }
 }
