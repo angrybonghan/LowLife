8ce3b54d5a3dbe86055c91d2ee1b9ef3b0c24bc9
LSH005 <isihyeon005shingu@gmail.com>
2025-11-23 03:32:23 +0900

    [feat] 바닥의 재질에 따라 발소리가 달라지는 기능 구현

--- Assets/Script/Entity/Player/PlayerSound.cs
+++ Assets/Script/Entity/Player/PlayerSound.cs
@@ -1,12 +1,15 @@
+using System.Drawing;
 using UnityEngine;
-using UnityEngine.UIElements;
+
+public enum playerSoundType { FootStep, Throw, Jump, Dash, Parry, QuickTrun };
 
 public class PlayerSound : MonoBehaviour
 {
-    public enum soundType { FootStep, Throw ,Jump, Dash, Parry, QuickTrun };
-
     [Header("¹ß¼Ò¸®")]
-    public AudioClip[] footStepSound;
+    public LayerMask groundMask;
+    public AudioClip[] grassFootStepSound;
+    public AudioClip[] stoneFootStepSound;
+    public AudioClip[] woodFootStepSound;
 
     [Header("Äü ÅÏ")]
     public AudioClip[] quickTrunSound;
@@ -25,22 +28,21 @@ public class PlayerSound : MonoBehaviour
         
     }
 
-    public void PlaySound(soundType type)
+    public void PlaySound(playerSoundType type)
     {
         AudioClip clip = null;
         int randomIndex = 0;
 
         switch (type)
         {
-            case soundType.FootStep:
-                randomIndex = Random.Range(0, footStepSound.Length);
-                clip = footStepSound[randomIndex];
+            case playerSoundType.FootStep:
+                clip = GetMaterialFootstepSound();
                 break;
-            case soundType.Throw:
+            case playerSoundType.Throw:
                 randomIndex = Random.Range(0, throwSound.Length);
                 clip = throwSound[randomIndex];
                 break;
-            case soundType.QuickTrun:
+            case playerSoundType.QuickTrun:
                 randomIndex = Random.Range(0, quickTrunSound.Length);
                 clip = quickTrunSound[randomIndex];
                 break;
@@ -50,5 +52,42 @@ public class PlayerSound : MonoBehaviour
         {
             SoundManager.instance.PlaySoundAtPosition(transform.position, clip);
         }
+    }
+
+    AudioClip GetMaterialFootstepSound()
+    {
+        const float MAX_RAY_DISTANCE = 1.0f;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, MAX_RAY_DISTANCE, groundMask);
+
+        if (hit.collider != null)
+        {
+            int randomIndex;
+
+            if (hit.collider.gameObject.TryGetComponent<FloorMaterial>(out FloorMaterial floor))
+            {
+                switch (floor.material)
+                {
+                    case materialEnum.grass:
+                        randomIndex = Random.Range(0, grassFootStepSound.Length);
+                        return grassFootStepSound[randomIndex];
+                    case materialEnum.stone:
+                        randomIndex = Random.Range(0, stoneFootStepSound.Length);
+                        return stoneFootStepSound[randomIndex];
+                    case materialEnum.wood:
+                        randomIndex = Random.Range(0, woodFootStepSound.Length);
+                        return woodFootStepSound[randomIndex];
+                    default:
+                        randomIndex = Random.Range(0, grassFootStepSound.Length);
+                        return grassFootStepSound[randomIndex];
+                }
+            }
+            else
+            {
+                randomIndex = Random.Range(0, grassFootStepSound.Length);
+                return grassFootStepSound[randomIndex];
+            }
+        }
+
+        return null;
     }
 }
---
+++ Assets/Script/Object/FloorMaterial.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public enum materialEnum { grass, stone, wood }
+
+public class FloorMaterial : MonoBehaviour
+{
+    public materialEnum material;
+}
