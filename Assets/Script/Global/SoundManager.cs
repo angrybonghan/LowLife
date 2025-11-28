ceade103778f91170eac61859ffed5ca5760bf82
LSH005 <isihyeon005shingu@gmail.com>
2025-11-29 02:30:22 +0900

    [feat] BGM 재생 구현

--- Assets/Script/Global/SoundManager.cs
+++ Assets/Script/Global/SoundManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
@@ -5,13 +6,11 @@ public class SoundManager : MonoBehaviour
 {
     public static SoundManager instance { get; private set; }
 
-    private static float Name;
-
-
     [Header("엔티티 타격음")]
     public AudioClip[] entityHitSound;
 
     float volume = 1.0f;
+    List<GameObject> allBgmPlayer = new List<GameObject>();
 
     private void Awake()
     {
@@ -31,6 +30,24 @@ public class SoundManager : MonoBehaviour
         {
             AudioSource.PlayClipAtPoint(clip, position, volume * volumeMultiple);
         }
+    }
+
+    public void PlayRandomSoundAtPosition(Vector3 position, AudioClip[] clips, float volumeMultiple = 1f)
+    {
+        if (clips == null || clips.Length == 0) return;
+
+        AudioClip clip = null;
+        int randomIndex = Random.Range(0, clips.Length);
+        clip = clips[randomIndex];
+        AudioSource.PlayClipAtPoint(clip, position, volume * volumeMultiple);
+    }
+
+    public AudioClip GetRandomSound(AudioClip[] clips)
+    {
+        if (clips == null || clips.Length == 0) return null;
+
+        int randomIndex = Random.Range(0, clips.Length);
+        return clips[randomIndex];
     }
 
     public void PlayEntityHitSound(Vector3 position)
@@ -61,5 +78,51 @@ public class SoundManager : MonoBehaviour
 
         audioSource.Play();
         Destroy(tempGO, clip.length / Mathf.Abs(pitch));
+    }
+
+    public void PlayRandomClipAtPointWithPitch(Vector3 position, AudioClip[] clips, float pitch = 1.0f)
+    {
+        if (clips == null || clips.Length == 0) return;
+
+        int randomIndex = Random.Range(0, clips.Length);
+        AudioClip clip = clips[randomIndex];
+
+        GameObject tempGO = new GameObject("TempAudio");
+        tempGO.transform.position = position;
+
+        AudioSource audioSource = tempGO.AddComponent<AudioSource>();
+        audioSource.clip = clip;
+        audioSource.spatialBlend = 1.0f;
+
+        audioSource.volume = volume;
+        audioSource.pitch = pitch;
+
+        audioSource.Play();
+        Destroy(tempGO, clip.length / Mathf.Abs(pitch));
+    }
+
+    public void PlayLoopBgm(AudioClip clip, float pitch = 1.0f)
+    {
+        GameObject tempBgmGO = new GameObject("TempBgmAudio");
+        allBgmPlayer.Add(tempBgmGO);
+
+        AudioSource audioSource = tempBgmGO.AddComponent<AudioSource>();
+        audioSource.clip = clip;
+        audioSource.spatialBlend = 0f;
+        audioSource.volume = volume;
+        audioSource.pitch = pitch;
+        audioSource.loop = true;
+
+        audioSource.Play();
+    }
+
+    public void StopAllBgm()
+    {
+        foreach (var player in allBgmPlayer)
+        {
+            Destroy(player);
+        }
+
+        allBgmPlayer.Clear();
     }
 }
