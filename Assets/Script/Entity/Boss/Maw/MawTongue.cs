295b8f4e783c6790dc6321316c2785bbdcc50880
LSH005 <isihyeon005shingu@gmail.com>
2025-12-02 03:15:06 +0900

    [feat] Maw의 혀 공격 구현

---
+++ Assets/Script/Entity/Boss/Maw/MawTongue.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class MawTongue : MonoBehaviour
+{
+    [Header("목표")]
+    public float backoffY;
+    public float targetY;
+    public float floorY;
+
+    [Header("속도")]
+    public float attackTime = 0.2f;
+    public float backoffTime = 0.5f;
+
+    [Header("공격 시간")]
+    public int phaseCount = 1;
+    public float aimingTime = 3f;
+
+    bool isAttacking = false;
+    Transform target;
+    Coroutine moveCoroutine;
+    Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Start()
+    {
+        target = PlayerController.instance.transform;
+        Vector3 startPos = Vector3.zero;
+        startPos.x = target.position.x;
+        startPos.y = backoffY;
+        transform.position = startPos;
+
+        StartCoroutine(SkillSequence());
+    }
+
+    IEnumerator SkillSequence()
+    {
+        for (int i = 0; i < phaseCount; i++)
+        {
+            Vector2 targetPos = transform.position;
+            targetPos.y = targetY;
+
+            MoveTo(targetPos, 0.3f);
+            yield return new WaitForSeconds(0.3f);
+
+            float time = 0;
+            while (time < aimingTime)
+            {
+                time += Time.deltaTime;
+                targetPos.x = target.position.x;
+                transform.position = Vector2.Lerp(transform.position, targetPos, 0.6f);
+                yield return null;
+            }
+
+            targetPos.y = floorY;
+            isAttacking = true;
+            MoveTo(targetPos, attackTime);
+            yield return new WaitForSeconds(attackTime);
+            isAttacking = false;
+            targetPos.y = backoffY;
+            anim.SetTrigger("attack");
+            MoveTo(targetPos, backoffTime);
+            yield return new WaitForSeconds(backoffTime);
+            anim.SetTrigger("default");
+        }
+
+        Destroy(gameObject);
+    }
+
+    void MoveTo(Vector2 targetPos, float duration)
+    {
+        if (moveCoroutine != null) StopCoroutine(moveCoroutine);
+        moveCoroutine = StartCoroutine(Co_MoveTo(targetPos, duration));
+    }
+
+    IEnumerator Co_MoveTo(Vector2 targetPos, float duration)
+    {
+        if (duration > 0)
+        {
+            float time = 0;
+            Vector2 startPos = transform.position;
+
+            while (time < duration)
+            {
+                yield return null;
+                transform.position = Vector2.Lerp(startPos, targetPos, time / duration);
+                time += Time.deltaTime;
+            }
+        }
+
+        transform.position = targetPos;
+        moveCoroutine = null;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!isAttacking) return;
+
+        if (collision.CompareTag("Player"))
+        {
+            PlayerController.instance.ImmediateDeath();
+        }
+    }
+}
--- Assets/Script/Entity/Boss/Maw/Maw_S4.cs
+++ Assets/Script/Entity/Boss/Maw/Maw_S4.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class Maw_S4 : MonoBehaviour, I_MawSkill
 {
     [Header("점프 목표")]
+    public float jumpPower = 25.0f;
     public float targetY;
 
+    [Header("중앙 X")]
+    public float centerX;
+
+    [Header("혀")]
+    public MawTongue tongue;
+    public float aimingTime;
+
     public bool isFacingRight { get; set; }
+    Vector2 fallDownPos;
 
     Animator anim;
     Rigidbody2D rb;
@@ -24,17 +33,51 @@ public class Maw_S4 : MonoBehaviour, I_MawSkill
         {
             transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
         }
-    }
 
-
-    void Update()
-    {
-        
+        StartCoroutine(SkillSequence());
     }
 
     IEnumerator SkillSequence()
     {
-        yield return null;
+        yield return new WaitUntil(() => transform.position.y >= targetY);
+
+        transform.position = new Vector2(7777, 1234);
+
+        
+
+    }
+
+    public void Jump()
+    {
+        LookPos(centerX);
+        int sigh = isFacingRight ? 1 : -1;
+
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(jumpPower * sigh, jumpPower * 2);
+
+        fallDownPos.x = centerX;
+        fallDownPos.x += transform.position.x >= centerX ? -5f : 5f;
+        fallDownPos.y = targetY;
+    }
+
+    public void FallDown()
+    {
+        transform.position = fallDownPos;
+        LookPos(centerX);
+        int sigh = isFacingRight ? 1 : -1;
+
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(jumpPower * sigh, jumpPower * -2);
+    }
+
+    void LookPos(float targetX)
+    {
+        float directionX = targetX - transform.position.x;
+
+        if (directionX != 0 && (directionX > 0) != isFacingRight)
+        {
+            Flip();
+        }
     }
 
     public void Flip()
