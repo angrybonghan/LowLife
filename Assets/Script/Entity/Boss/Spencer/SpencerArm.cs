79524b6e764024f4e183c89ccfc652fce2826528
LSH005 <isihyeon005shingu@gmail.com>
2025-12-04 08:28:23 +0900

    [feat] Spencer 팔 움직임, 총 발사 구현

--- Assets/Script/Entity/Boss/Spencer/SpencerArm.cs
+++ Assets/Script/Entity/Boss/Spencer/SpencerArm.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using UnityEngine;
 
-public class SpencerArm : MonoBehaviour
+[RequireComponent(typeof(Animator))]
+public class SpencerArm : MonoBehaviour, I_Attackable
 {
     [Header("팔 범위")]
     public float maxZAngle;
@@ -14,36 +15,154 @@ public class SpencerArm : MonoBehaviour
     [Header("방향")]
     public bool isFacingRight = true;
 
-    [Header("공격")]
-    public int weaponNumber; // 0,1,2 순으로 리볼버, 샷건, 로켓
-    public float aimingTime;
-
     [Header("공격 위치")]
     public Transform firePoint;
     public Transform aimPoint;
 
-    Transform targetPos;
+    [Header("투사체")] // 웨폰 넘버 0-1-2 순
+    public EnemyLaser revolver;
+    public ShotGunBullet shotgun;
+    public Ms_PatriotProjectile roket;
 
-    void Start()
+    [Header("놓칠 총")]
+    public GameObject revolverDrop;
+    public GameObject shotgunDrop;
+    public GameObject roketDrop;
+
+
+    [HideInInspector] public Spencer_S1 parents;
+    [HideInInspector] public bool sendSignal;
+    int weaponNumber;
+    bool gunDrop = false;
+    bool canGunDrop = false;
+    Animator anim;
+
+    private void Awake()
     {
-        targetPos = PlayerController.instance.transform;
+        anim = GetComponent<Animator>();
     }
 
-    void Update()
+    private void Start()
     {
+        weaponNumber = SpencerManager.Instance.randomWeaponNumber;
+        anim.SetInteger("weaponNumber", weaponNumber);
+    }
 
+    IEnumerator ArmMove()
+    {
+        float angle1 = maxZAngle;
+        float angle2 = minZAngle;
+
+        if (Random.value > 0.5f)
+        {
+            angle1 = minZAngle;
+            angle2 = maxZAngle;
+        }
+
+        while (true)
+        {
+            yield return StartCoroutine(RotateCoroutine(angle1, GetArmSpeed()));
+            yield return StartCoroutine(RotateCoroutine(angle2, GetArmSpeed()));
+        }
 
     }
 
-    //IEnumerator Sequence()
-    //{
-
-
-    //}
-
-    void WeaponFiring()
+    public void FireWeapon()
     {
-
+        StopAllCoroutines();
+        anim.SetTrigger("fire");
+        canGunDrop = false;
     }
 
+    public void EndMoveArms()
+    {
+        parents.EndMoveArms();
+    }
+
+    public void SummonProjectile()
+    {
+        if (gunDrop) return;
+
+        if (weaponNumber == 0)
+        {
+            EnemyLaser proj = Instantiate(revolver, firePoint.position, Quaternion.identity);
+            proj.SetTarget(aimPoint);
+            proj.shrinkTime = 0.1f;
+            proj.laserThickness = 0.1f;
+        }
+        else if (weaponNumber == 1)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                Instantiate(shotgun, firePoint.position, Quaternion.identity).LookPos(aimPoint.position);
+            }
+        }
+        else
+        {
+            Ms_PatriotProjectile proj = Instantiate(roket, firePoint.position, Quaternion.identity);
+            proj.SetFacing(aimPoint.position);
+        }
+    }
+
+    float GetArmSpeed()
+    {
+        return Random.Range(minArmMoveSpeed, maxArmMoveSpeed);
+    }
+
+    public void StartArmMove()
+    {
+        StartCoroutine(ArmMove());
+        if (sendSignal) parents.StartMoveArms();
+        canGunDrop = true;
+    }
+
+    public void Destruction()
+    {
+        Destroy(gameObject);
+    }
+
+    IEnumerator RotateCoroutine(float targetZ, float duration)
+    {
+        float timeElapsed = 0f;
+        float startZ = transform.eulerAngles.z;
+        Quaternion targetRotation = Quaternion.Euler(0, 0, targetZ);
+
+        while (timeElapsed < duration)
+        {
+            float t = timeElapsed / duration;
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, t);
+            yield return null;
+            timeElapsed += Time.deltaTime;
+        }
+
+        transform.rotation = targetRotation;
+    }
+
+    public bool CanAttack(Transform attacker)
+    {
+        return canGunDrop;
+    }
+
+    public void OnAttack(Transform attacker)
+    {
+        GetComponent<Collider2D>().enabled = false;
+        anim.SetTrigger("hit");
+        gunDrop = true;
+    }
+
+    public void GunDrop()
+    {
+        GameObject gunDrop = null;
+        if (weaponNumber == 0) gunDrop = revolverDrop;
+        else if (weaponNumber == 1) gunDrop = shotgunDrop;
+        else gunDrop = roketDrop;
+
+        GameObject gun = Instantiate(gunDrop, firePoint.position, Quaternion.identity);
+        if (!isFacingRight)
+        {
+            Vector2 scale = gun.transform.localScale;
+            scale.x *= -1f;
+            gun.transform.localScale = scale;
+        }
+    }
 }
