32971a6983be0993e5df9331e1b8b6e5d496c2ec
LSH005 <isihyeon005shingu@gmail.com>
2025-12-09 19:42:36 +0900

    [feat] Bead 튜토리얼 지형 구현

---
+++ Assets/Script/Global/TriggerBox/CameraParts/TBP_CameraZoom.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class TBP_CameraZoom : MonoBehaviour, I_TriggerBox
+{
+    public enum TriggerBoxPartActionType { atIn, atOut, both }
+    [Header("동작 시점")]
+    public TriggerBoxPartActionType actionType = TriggerBoxPartActionType.atIn;
+
+    [Header("카메라")]
+    public float cameraZoom = 17;
+    public float cameraMoveDuration = 0.5f;
+
+    public void TriggerIn()
+    {
+        if (actionType == TriggerBoxPartActionType.atOut) return;
+
+        Trigger();
+    }
+
+    public void TriggerOut()
+    {
+        if (actionType == TriggerBoxPartActionType.atIn) return;
+
+        Trigger();
+    }
+
+    void Trigger()
+    {
+        CameraMovement.PositionZoom(cameraZoom, cameraMoveDuration);
+    }
+}
