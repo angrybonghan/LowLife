4aa211fe4693bd3d86369039c0432b43b66f4c75
LSH005 <isihyeon005shingu@gmail.com>
2025-12-10 13:13:18 +0900

    [feat] 늙은 개미와 만남 컷신에 소리 구현

---
+++ Assets/UnityAnimation/Scene/Bar_1/Bar_1_Cut.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public enum Bar_1_SoundType { firing, block }
+
+public class Bar_1_Cut : MonoBehaviour
+{
+    [Header("√—")]
+    public AudioClip gunShotSound;
+
+    [Header("πÊ∆–")]
+    public AudioClip shieldBlockSound;
+
+
+    public void PlaySound(Bar_1_SoundType type)
+    {
+        AudioClip clip = null;
+        Vector3 camPos = Camera.main.transform.position;
+
+        switch (type)
+        {
+            case Bar_1_SoundType.firing:
+                clip = gunShotSound;
+                break;
+            case Bar_1_SoundType.block:
+                clip = shieldBlockSound;
+                break;
+        }
+
+        if (clip != null) SoundManager.instance.PlaySoundAtPosition(camPos, clip, 0.5f);
+    }
+}
