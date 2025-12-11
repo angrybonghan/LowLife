048d6bc28d21e5e30680224857be748c1405e50f
LSH005 <isihyeon005shingu@gmail.com>
2025-12-11 11:43:03 +0900

    [fix] 레터박스가 빌드에서 나오지 않는 문제 해결

--- Assets/Script/Global/Scene/LetterBoxController.cs
+++ Assets/Script/Global/Scene/LetterBoxController.cs
@@ -23,10 +23,7 @@ public class LetterBoxController : MonoBehaviour
             this.enabled = false;
             return;
         }
-    }
 
-    private void Start()
-    {
         if (disableAtStart)
         {
             SetEnableImmediate(false);
